ad0503bfab4a108c0b71c467865699c1d75c173b
tmsksfh2012 <tmsksfh2012@naver.com>
2021-12-01 21:13:08 +0900

    infinity_stairs

---
+++ infinity_stairs/.history/Assets/Scripts/GameManager_20211201211123.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Character;
+    public Transform Platform_Parents;
+    public GameObject Platform;
+    private List<GameObject> Platforn_List = new List<Game
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
